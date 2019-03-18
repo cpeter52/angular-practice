d1b7708b5693ad675cd755724b0fa9d6bbfe3550
Chris Peterson <petersonchris1216@gmail.com>
2019-03-18 08:03:37 -0400

    Favorite button and title casing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { FormsModule } from '@angular/forms';
+import { TitleCasePipe } from './title-case.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CoursesService,
